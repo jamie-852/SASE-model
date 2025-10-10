b299275dd050635aa4b11b072ec2699b7a5e08cf
jamie-852 <lee.jczx@gmail.com>
2025-10-10 14:24:20 +0100

    Deleted files

--- Violin plots/Info.m
+++
@@ -1,6 +0,0 @@
-%% g_ViolinPlot_combined.m in folder Figure S2
-% reproduces violin plot in Figure S2.
-% Figure 2d was taken from these plots. 
-
-%% g_ViolinPlot_nd.m and g_ViolinPlot_d.m in folder Figure S3
-% reproduces violin plots (a) and (b) respectively in Figure S3.
